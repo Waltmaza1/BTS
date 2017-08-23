833a3b0bf35b1f81be885ec7e8b8f03f69d5f7ec
Nithya Sam <nithyasam@nithyas-air.gateway.pace.com>
2017-08-23 00:01:52 -0500

    updated the server config

--- server.js
+++ server.js
@@ -1,44 +1,29 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var apiRoutes = require('./routes/api-routes.js');
-var htmlRoutes = require('./routes/html-routes.js');
 var app = express();
-var exphbs = require("express-handlebars");
-var db = require("./models");
-var connection = require("./config/connection.js")
-var orm = require("./config/orm.js");
-
 var PORT = process.env.PORT || 3000;
-
+// Requiring our models for syncing
+var db = require("./models");
+var connection = require("./config/connection.js");
+var exphbs = require("express-handlebars");
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(methodOverride("_method"));
+app.use(bodyParser.text());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./test/route-test.js")(app);
 
-apiRoutes(app);
-htmlRoutes(app);
-app.get("/shipping", function(req,res){
-		console.log("something");
-	orm.getShipping(function(callback){
-		console.log(callback);
-		res.render("shipping", { data: callback});
-	});
-});
-
-
-db.sequelize.sync({}).then( function(){
-	app.listen(PORT, function() {
-    console.log("I got you on 3000");
-});
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
